03878ca5f328f7c0fb2da2dbba404c50d1b67a78
Ganesh <ganeshgarje716@gmail.com>
2026-05-24 22:56:28 +0530

    Create Classes Config, Employee, Test

---
+++ Spring-6/src/main/java/com/Ganesh/entity/Employee.java
@@ -0,0 +1,14 @@
+package com.Ganesh.entity;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+	
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Employee Construcor");
+	}
+
+}
