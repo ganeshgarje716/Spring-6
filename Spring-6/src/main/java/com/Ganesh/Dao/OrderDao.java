6f0adde68a558a2ca72eeeebe7e9c5a794a6f96e
Ganesh <ganeshgarje716@gmail.com>
2026-05-26 12:12:10 +0530

    Create Classes OrderDao, OrderService

---
+++ Spring-6/src/main/java/com/Ganesh/Dao/OrderDao.java
@@ -0,0 +1,15 @@
+package com.Ganesh.Dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class OrderDao {
+	
+	
+	
+	public void getOrder() {
+		
+		System.out.println("Getting Order Form DB");
+	}
+
+}
---
+++ Spring-6/src/main/java/com/Ganesh/Service/OrderService.java
@@ -0,0 +1,25 @@
+package com.Ganesh.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Ganesh.Dao.OrderDao;
+
+@Service
+public class OrderService {
+	
+	
+	@Autowired
+	OrderDao dao;
+	
+	@Autowired
+	Payment payment;
+	
+	public void processOrder() {
+		
+		dao.getOrder();
+		payment.pay(2000);
+		System.out.println("Order Processed...✅");
+	}
+
+}
